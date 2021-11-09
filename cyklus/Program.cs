a19840789c93ed3c507152a462c161b8de9228ae
Patrik Sigl <Sigl.Patrik@stud.vassboskovice.cz>
2021-11-09 11:35:12 +0100

    Přidejte soubory projektu.

---
+++ cyklus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cyklus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int cislo = 1;
+            while (cislo <= 10)
+                {
+                Console.WriteLine(cislo);
+                cislo++;
+                //cislo = cislo = 1;
+                //cislo += 1;
+            }
+            Console.ReadLine();
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
